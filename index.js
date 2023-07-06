5328d15cfcf5cab46896ebdb34ecb526e877ce32
ENG-CJ <phonereserved7@gmail.com>
2023-07-06 16:14:41 +0300

    fix 2

--- index.js
+++ index.js
@@ -5,21 +5,11 @@ const multer = require("multer");
 const app = express();
 const cors = require("cors");
 
-
+app.use(cors({origin: "*"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 require("dotenv").config();
-
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 
 app.get("/", (req, res) => {
   return res.json({ message: "Server is running and ready to cook" });
