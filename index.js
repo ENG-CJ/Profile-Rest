74108be3062cbfec1d9151211fb72e3156b55e15
ENG-CJ <phonereserved7@gmail.com>
2023-07-06 09:52:42 +0300

    changes cors

--- index.js
+++ index.js
@@ -4,36 +4,24 @@ const multer=require("multer");
 
 const app = express();
 const cors = require('cors');
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(express.static("public"));
 
 require('dotenv').config();
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://hirehero.000webhostapp.com"
-    );
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', '*');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 
 app.get("/", (req, res) => {
