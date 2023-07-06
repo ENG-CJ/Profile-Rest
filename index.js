55dc85a209adb0e697c0b6f49359d5422be0fb6a
ENG-CJ <phonereserved7@gmail.com>
2023-07-06 08:50:15 +0300

    allow headers

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ app.use(express.static("public"));
 
 require('dotenv').config();
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://hirehero.000webhostapp.com"
+    );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.get("/", (req, res) => {
   return res.json({ message: "Server is running and ready to cook" });
 });
